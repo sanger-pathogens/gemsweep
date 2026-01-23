bce2f853b80b73e8faffbe226d50e110967bd460
Tom Morrison <tm22@sanger.ac.uk>
2026-01-23 14:14:59 +0000

    update themisto runtime and produce working poppunk clustering module

--- bin/sketchlib_helper.py
+++ bin/sketchlib_helper.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
     with open(outdir / 'references.tsv', 'w') as out_f:
         for fasta in file:
             path = Path(fasta)
-            sample = path.stem
+            sample = Path(path.stem).with_suffix('')
             out_f.write(f'{sample}\t{path}\n')
