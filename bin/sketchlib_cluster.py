296962c508e886499092bad806d413c9d6f4e5cd
cc52 <cc52@sanger.ac.uk>
2026-05-11 10:50:49 +0100

    Update docstring

--- bin/sketchlib_cluster.py
+++ bin/sketchlib_cluster.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 """
-Cluster genomes in a pre-sketched sketchlib database by pairwise ANI distance 
-using connected components. Outputs a CSV mapping each genome ID to a cluster 
-ID. Also performs cluster checks; if each genome is in its own cluster or all 
-genomes fall into a single cluster, log a warning or, in strict mode, exit with
-nonzero exit code.
+Cluster genomes in a pre-sketched sketchlib database by pairwise ANI distance
+using clustering/ community-finding algorithm of choice. Outputs a CSV mapping
+each genome ID to a cluster ID. Also sanity checks clusters; if each genome
+is in it's own cluster or all genomes fall into a single cluster, it either logs
+a warning or, in strict mode, fails with a non-zero exit code.
 
 Example usage:
     sketchlib_cluster.py \
